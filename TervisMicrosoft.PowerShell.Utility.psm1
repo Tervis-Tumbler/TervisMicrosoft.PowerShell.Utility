3beaa12d0d6612ba9cd4786511b065e6448256ce
Chris Magnuson <hourglasssand@gmail.com>
2017-05-01 09:17:21 -0400

    Initial commit

---
+++ TervisMicrosoft.PowerShell.Utility.psm1
@@ -0,0 +1,16 @@
+﻿function ConvertFrom-StringUsingRegexCaptureGroup {
+    param (
+        [Regex]$Regex,
+        [Parameter(ValueFromPipeline)]$Content
+    )
+    process {
+        $Match = $Regex.Match($Content)
+        $Object = [pscustomobject]@{} 
+        
+        foreach ($GroupName in $Regex.GetGroupNames() | select -Skip 1) {
+            $Object | 
+            Add-Member -MemberType NoteProperty -Name $GroupName -Value $Match.Groups[$GroupName].Value 
+        }
+        $Object
+    }
+}
