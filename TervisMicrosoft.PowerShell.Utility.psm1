c3a8958ac548d1bf51efdf7b963092ff3cd160f9
ChrisMagnuson <hourglasssand@gmail.com>
2017-07-20 17:16:40 -0400

    Move ConvertTo-HashTable to appropriate module

--- TervisMicrosoft.PowerShell.Utility.psm1
+++ TervisMicrosoft.PowerShell.Utility.psm1
@@ -33,3 +33,13 @@ function Compare-ObjectAllProperties {
 
     Compare-Object -ReferenceObject $ReferenceObject -DifferenceObject $DifferenceObject -Property $Properties -IncludeEqual:$IncludeEqual
 }
+
+function ConvertTo-HashTable {
+    #Inspired by http://stackoverflow.com/questions/3740128/pscustomobject-to-hashtable
+    param(
+        [Parameter(ValueFromPipeline)]$Object
+    )
+    $HashTable = @{}
+    $Object.psobject.properties | Foreach { $HashTable[$_.Name] = $_.Value }
+    $HashTable
+}
