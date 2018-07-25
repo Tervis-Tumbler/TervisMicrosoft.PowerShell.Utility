63d5495977f89904b4fe3e2ad70cf8de7e75a7cb
ChrisMagnuson <hourglasssand@gmail.com>
2018-07-25 16:10:16 -0400

    Add attempt to genearlize caching scriptproperties

--- TervisMicrosoft.PowerShell.Utility.psm1
+++ TervisMicrosoft.PowerShell.Utility.psm1
@@ -910,3 +910,132 @@ Function New-DynamicParameter {
 		}
 	}
 }
+
+#Use the following to generate the proxy function:
+#$MetaData = New-Object System.Management.Automation.CommandMetaData (Get-Command Add-Member) 
+#[System.Management.Automation.ProxyCommand]::Create($MetaData)
+
+function Add-TervisMemeber {
+	[CmdletBinding(DefaultParameterSetName='TypeNameSet', HelpUri='https://go.microsoft.com/fwlink/?LinkID=113280', RemotingCapability='None')]
+	param(
+		[Parameter(ParameterSetName='MemberSet', Mandatory=$true, ValueFromPipeline=$true)]
+		[Parameter(ParameterSetName='TypeNameSet', Mandatory=$true, ValueFromPipeline=$true)]
+		[Parameter(ParameterSetName='NotePropertySingleMemberSet', Mandatory=$true, ValueFromPipeline=$true)]
+		[Parameter(ParameterSetName='NotePropertyMultiMemberSet', Mandatory=$true, ValueFromPipeline=$true)]
+		[psobject]
+		${InputObject},
+
+		[Parameter(ParameterSetName='MemberSet', Mandatory=$true, Position=0)]
+		[Alias('Type')]
+		[System.Management.Automation.PSMemberTypes]
+		${MemberType},
+
+		[Parameter(ParameterSetName='MemberSet', Mandatory=$true, Position=1)]
+		[string]
+		${Name},
+
+		[Parameter(ParameterSetName='MemberSet', Position=2)]
+		[System.Object]
+		${Value},
+
+		[Parameter(ParameterSetName='MemberSet', Position=3)]
+		[System.Object]
+		${SecondValue},
+
+		[Parameter(ParameterSetName='MemberSet')]
+		[Parameter(ParameterSetName='TypeNameSet', Mandatory=$true)]
+		[Parameter(ParameterSetName='NotePropertySingleMemberSet')]
+		[Parameter(ParameterSetName='NotePropertyMultiMemberSet')]
+		[ValidateNotNullOrEmpty()]
+		[string]
+		${TypeName},
+
+		[Parameter(ParameterSetName='MemberSet')]
+		[Parameter(ParameterSetName='NotePropertySingleMemberSet')]
+		[Parameter(ParameterSetName='NotePropertyMultiMemberSet')]
+		[switch]
+		${Force},
+
+		[Parameter(ParameterSetName='NotePropertySingleMemberSet')]
+		[Parameter(ParameterSetName='TypeNameSet')]
+		[Parameter(ParameterSetName='MemberSet')]
+		[Parameter(ParameterSetName='NotePropertyMultiMemberSet')]
+		[switch]
+		${PassThru},
+
+		[Parameter(ParameterSetName='NotePropertySingleMemberSet', Mandatory=$true, Position=0)]
+		[ValidateNotNullOrEmpty()]
+		[string]
+		${NotePropertyName},
+
+		[Parameter(ParameterSetName='NotePropertySingleMemberSet', Mandatory=$true, Position=1)]
+		[AllowNull()]
+		[System.Object]
+		${NotePropertyValue},
+
+		[Parameter(ParameterSetName='NotePropertyMultiMemberSet', Mandatory=$true, Position=0)]
+		[ValidateNotNullOrEmpty()]
+		[System.Collections.IDictionary]
+		${NotePropertyMembers}
+	)
+	DynamicParam {
+		$DynamicParameters = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
+		if ($MemberType -eq "ScriptProperty") {
+			New-DynamicParameter -Name CacheValue -Type switch -Dictionary $DynamicParameters
+		}
+		$DynamicParameters
+	}
+
+	begin
+	{
+		try {
+			$outBuffer = $null
+			if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer))
+			{
+				$PSBoundParameters['OutBuffer'] = 1
+			}
+			$wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Utility\Add-Member', [System.Management.Automation.CommandTypes]::Cmdlet)
+
+			if ($CacheValue) {
+				$PSBoundParameters.Value = [ScriptBlock]::Create(@"
+					`$This | Add-Member -MemberType NoteProperty -Name $Name -Force -Value `$(
+						$($Value.ToString())
+					)
+					`$This.$Name
+"@
+				)
+			}
+
+			$scriptCmd = {& $wrappedCmd @PSBoundParameters }
+
+			$steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin)
+			$steppablePipeline.Begin($PSCmdlet)
+		} catch {
+			throw
+		}
+	}
+
+	process
+	{
+		try {
+			$steppablePipeline.Process($_)
+		} catch {
+			throw
+		}
+	}
+
+	end
+	{
+		try {
+			$steppablePipeline.End()
+		} catch {
+			throw
+		}
+	}
+	<#
+
+	.ForwardHelpTargetName Microsoft.PowerShell.Utility\Add-Member
+	.ForwardHelpCategory Cmdlet
+
+	#>
+}
